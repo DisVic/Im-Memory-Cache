1304481ad79397e45b94feff6b994713b9889248
DisVic <rogueone200534@gmail.com>
2024-07-18 18:59:17 +0300

    .

--- inMemoryCache/inMemoryCache.go
+++ inMemoryCache/inMemoryCache.go
@@ -18,18 +18,18 @@ func (c *Cache) Set(key string, value interface{}) {
 	c.data[key] = value
 }
 
-func (c *Cache) Get(key string) (interface{}, error)  {
+func (c *Cache) Get(key string) (interface{}, error) {
 	value, ok := c.data[key]
-	if !ok{
-		return nil, errors.New("Не найден")
+	if !ok {
+		return nil, errors.New("не найден")
 	}
-	return value, nil	 
+	return value, nil
 }
 
 func (c *Cache) Delete(key string) error {
 	_, ok := c.data[key]
-	if !ok{
-		return errors.New("Не найден")
+	if !ok {
+		return errors.New("не найден")
 	}
 	delete(c.data, key)
 	return nil
