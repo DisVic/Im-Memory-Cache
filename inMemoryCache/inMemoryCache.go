b9e57e883b0d13a008ff27385e8b2ab2673661f1
DisVic <rogueone200534@gmail.com>
2024-07-25 23:25:09 +0300

    ,

--- inMemoryCache/inMemoryCache.go
+++ inMemoryCache/inMemoryCache.go
@@ -2,41 +2,79 @@ package cache
 
 import (
 	"errors"
+	"sync"
+	"time"
 
 	"golang.org/x/exp/maps"
 )
 
-type Cache struct {
-	data map[string]interface{}
+type Cache[K comparable, T any] struct {
+	mu   sync.RWMutex
+	data map[K]CacheItem[T]
 }
 
-func NewCache() *Cache {
-	return &Cache{
-		data: make(map[string]interface{}),
+type CacheItem[T any] struct {
+	value  T
+	expiry time.Time
+}
+
+func NewCache[K comparable, T any]() *Cache[K, T] {
+	return &Cache[K, T]{
+		data: make(map[K]CacheItem[T]),
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
-	c.data[key] = value
-}
-
-func (c *Cache) Get(key string) (interface{}, error) {
-	value, ok := c.data[key]
-	if !ok {
-		return nil, errors.New("не найден")
-	}
-	return value, nil
-}
-
-func (c *Cache) Delete(key string) error {
+func (c *Cache[K, T]) deleteAfterExpiration(key K, ttl time.Duration) {
+	time.Sleep(ttl)
 	_, ok := c.data[key]
-	if !ok {
+	if ok && c.data[key].expiry.Before(time.Now()) {
+		c.mu.Lock()
+		delete(c.data, key)
+		c.mu.Unlock()
+	}
+}
+
+func (c *Cache[K, T]) Set(key K, value T, ttl time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.data[key] = CacheItem[T]{
+		value:  value,
+		expiry: time.Now().Add(ttl),
+	}
+	go c.deleteAfterExpiration(key, ttl)
+}
+
+func (c *Cache[K, T]) Get(key K) (T, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	if value, ok := c.data[key]; !ok {
+		return zeroValue[T](), errors.New("не найден")
+	} else {
+		if value.expiry.Before(time.Now()) {
+			delete(c.data, key)
+			return zeroValue[T](), errors.New("срок жизни кэша истёк")
+		}
+		return value.value, nil
+	}
+}
+
+func zeroValue[T any]() T {
+	var value T
+	return value
+}
+
+func (c *Cache[K, T]) Delete(key K) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if _, ok := c.data[key]; !ok {
 		return errors.New("не найден")
 	}
 	delete(c.data, key)
 	return nil
 }
 
-func (c *Cache) Clear() {
+func (c *Cache[K, T]) Clear() {
+	c.mu.Lock()
 	maps.Clear(c.data)
+	c.mu.Unlock()
 }
